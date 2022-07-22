4626dd20eb2b358df6f2f03c53f28cf1676441be
Alexa Hendry <alexahendry88@gmail.com>
2022-07-22 17:46:34 +0000

    Add addTurn function and testing

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,4 +1,4 @@
-const { game, newGame, showScore } = require("../game");
+const { game, newGame, showScore, addTurn } = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -40,8 +40,8 @@ describe("newGame works correctly", () => {
     test("should clear playerMoves array", () => {
         expect(game.playerMoves.length).toEqual(0);
     });
-    test("should clear currentGame array", () => {
-        expect(game.currentGame.length).toEqual(0);
+    test("should be one move in the computer's game array", () => {
+        expect(game.currentGame.length).toEqual(1);
     });
     test("should display 0 for the element with the id of score", () => {
         expect(document.getElementById("score").innerText).toEqual(0);
