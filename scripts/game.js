aafaa7ae40c517b59ce671b3c9526a7138d2cd24
Alexa Hendry <alexahendry88@gmail.com>
2022-07-25 17:09:53 +0000

    Add lastButton and turnInProgress

--- scripts/game.js
+++ scripts/game.js
@@ -4,19 +4,24 @@ let game = {
     playerMoves: [],
     turnNumber: 0,
     choices: ["button1", "button2", "button3", "button4"],
+    lastButton: "",
+    turnInProgress: false,
 };
 
 function newGame() {
     game.score = 0;
-    playerMoves = [];
-    currentGame = [];
+    game.playerMoves = [];
+    game.currentGame = [];
     for (let circle of document.getElementsByClassName("circle")) {
         if (circle.getAttribute("data-listener") !== "true") {
             circle.addEventListener("click", (e) => {
-                let move = e.target.getAttribute("id");
-                lightsOn(move);
-                game.playerMoves.push(move);
-                playerTurn();
+                if (game.currentGame.length > 0 && !game.turnInProgress) {
+                    let move = e.target.getAttribute("id");
+                    game.lastButton = move;
+                    lightsOn(move);
+                    game.playerMoves.push(move);
+                    playerTurn();
+                }
             });
             circle.setAttribute("data-listener", "true");
         };
@@ -43,12 +48,14 @@ function lightsOn(circ) {
 };
 
 function showTurns() {
+    game.turnInProgress = true;
     game.turnNumber = 0;
     let turns = setInterval(() => {
         lightsOn(game.currentGame[game.turnNumber]);
         game.turnNumber++;
         if (game.turnNumber >= game.currentGame.length) {
             clearInterval(turns);
+            game.turnInProgress = false;
         }
     }, 800);
 };
